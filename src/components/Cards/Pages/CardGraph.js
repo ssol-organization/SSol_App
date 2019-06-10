ac3a6c121d5d93d895da140cdb58a078d524d78d
joao15victor08 <joao15victor08@gmail.com>
2019-06-09 21:16:40 -0300

    Add text to buttons

--- src/components/Cards/Pages/CardGraph.js
+++ src/components/Cards/Pages/CardGraph.js
@@ -5,6 +5,7 @@ import styles from '../styles/styles.js';
 
 const nyanCat = require('../../../images/nyanCat.gif')
 const sealJudging = require('../../../images/sealJudging.gif')
+var buttonsText = {0:"resposta 1", 1:"resposta 2", 2:"resposta 3"};
 
 export default class CardGraph extends Component{
   constructor(){
@@ -16,9 +17,15 @@ export default class CardGraph extends Component{
       buttonAnswer1: false,
       buttonAnswer2: false,
       buttonAnswer3: false,
-      buttonNext: false
-    };
+      buttonNext: false,
+      botao1:"",
+      botao2:"",
+      botao3:"",
+      cardTitle:"Questão ",
+      count:1
+    }
   }
+
 
   componentDidMount() {
     Animated.timing(
@@ -35,14 +42,24 @@ export default class CardGraph extends Component{
     switch (num) {
       case 0:
         this.setState({buttonAnswer1: true})
+        this.setState({botao1:buttonsText[num+1]})
+        this.setState({botao2:buttonsText[num]})
+        this.setState({botao3:buttonsText[num+2]})
         break;
       case 1:
         this.setState({buttonAnswer2: true})
+        this.setState({botao1:buttonsText[num+1]})
+        this.setState({botao2:buttonsText[num]})
+        this.setState({botao3:buttonsText[num-1]})
         break;
       case 2:
         this.setState({buttonAnswer3: true})
+        this.setState({botao1:buttonsText[num-2]})
+        this.setState({botao2:buttonsText[num]})
+        this.setState({botao3:buttonsText[num-1]})
         break;
     };
+
   }
   buttonPress1 = () => {
     if(this.state.pressed === false){
@@ -98,27 +115,37 @@ export default class CardGraph extends Component{
       buttonAnswer1: false,
       buttonAnswer2: false,
       buttonAnswer3: false,
-      pressed: false
-    });
+      pressed: false,
+      count:this.state.count+1
+    })
 
     let num = Math.random()*3;
     num = Math.floor(num);
     switch (num) {
       case 0:
         this.setState({buttonAnswer1: true})
+        this.setState({botao1:buttonsText[num+1]})
+        this.setState({botao2:buttonsText[num]})
+        this.setState({botao3:buttonsText[num+2]})
         break;
       case 1:
         this.setState({buttonAnswer2: true})
+        this.setState({botao1:buttonsText[num+1]})
+        this.setState({botao2:buttonsText[num]})
+        this.setState({botao3:buttonsText[num-1]})
         break;
       case 2:
         this.setState({buttonAnswer3: true})
+        this.setState({botao1:buttonsText[num-2]})
+        this.setState({botao2:buttonsText[num]})
+        this.setState({botao3:buttonsText[num-1]})
         break;
-    };
-
+    };;
   };
 
   render() {
     let { fadeAnim } = this.state;
+
     return (
       <Animated.View
           style={{
@@ -127,7 +154,7 @@ export default class CardGraph extends Component{
           }}
         >
         <View style={styles.mainCard}>
-          <Text style = {styles.cardTitle}>Diagramas</Text>
+          <Text style = {styles.cardTitle}>{this.state.cardTitle+this.state.count}</Text>
           <ScrollView style={styles.contentCard}>
             <View style={{justifyContent:'space-between'}}>
               <TouchableOpacity
@@ -135,21 +162,21 @@ export default class CardGraph extends Component{
                   ((this.state.pressed === true && this.state.buttonAnswer1 === false)?styles.botaoErrado:
                   styles.botaoQualquer)}
                 onPress={this.buttonPress1}>
-                <Text style={styles.cardTitle}>Botão 1</Text>
+                <Text style={styles.cardTitle}>{this.state.botao1}</Text>
               </TouchableOpacity>
               <TouchableOpacity
                 style={(this.state.pressed === true && this.state.buttonAnswer2 === true)?styles.botaoCerto:
                   ((this.state.pressed === true && this.state.buttonAnswer2 === false)?styles.botaoErrado:
                   styles.botaoQualquer)}
                 onPress={this.buttonPress2}>
-                <Text style={styles.cardTitle}>Botão 2</Text>
+                <Text style={styles.cardTitle}>{this.state.botao2}</Text>
               </TouchableOpacity>
               <TouchableOpacity
                 style={(this.state.pressed === true && this.state.buttonAnswer3 === true)?styles.botaoCerto:
                   ((this.state.pressed === true && this.state.buttonAnswer3 === false)?styles.botaoErrado:
                   styles.botaoQualquer)}
                 onPress={this.buttonPress3}>
-                <Text style={styles.cardTitle}>Botão 3</Text>
+                <Text style={styles.cardTitle}>{this.state.botao3}</Text>
               </TouchableOpacity>
               <View>
                 {this.state.pressed === false ? null:
